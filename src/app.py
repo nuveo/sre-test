865dc1ac596a9b4f46b980182d5bd02439cb8687
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2020-08-24 16:44:18 -0300

    finish api

--- src/app.py
+++ src/app.py
@@ -5,59 +5,74 @@ db = SQLAlchemy()
 
 # Models
 
-class BaseModel(db.Model):
+class Payment(db.Model):
+
+    INSERTED = "inserted"
+    CONSUMED = "consumed"
 
     id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(10))
+
+    def set_inserted(self):
+        self.status = self.INSERTED
+
+    def set_consumed(self):
+        self.status = self.CONSUMED
 
     def to_json(self):
-        return { "id": self.id }
+        return { "id": self.id, "status": self.status }
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
+# API functions
+
 @app.route('/create', methods=['POST'])
 def create():
     data = request.get_json()
-    bm = BaseModel(**data)
-    db.session.add(bm)
+    p = Payment(**data)
+    db.session.add(p)
     db.session.commit()
-    return jsonify(bm.to_json())
+    return jsonify(p.to_json())
 
 
-@app.route('/read/<id>', methods=['GET'])
-def read(id):
-    bm = BaseModel.query.get(id)
-
-    return jsonify(bm.to_json())
+@app.route('/list', methods=['GET'])
+def list():
+    ps = Payment.query.all()
+    return jsonify([p.to_json() for p in ps])
 
 
 @app.route('/update/<id>', methods=['PUT'])
 def update(id):
     data = request.get_json()
-    bm = BaseModel.query.get(id)
-    for key, value in data.items():
-        setattr(bm, key, value)
-    db.session.add(bm)
+    p = Payment.query.get(id)
+
+    new_status = data.get('status')
+
+    if new_status == Payment.INSERTED:
+        p.set_inserted()
+    elif new_status == Payment.CONSUMED:
+        p.set_consumed()
+
+    db.session.add(p)
     db.session.commit()
 
-    return jsonify(bm.to_json())
+    return jsonify(p.to_json())
 
 
 @app.route('/delete/<id>', methods=['DELETE'])
 def delete(id):
-    bm = BaseModel.query.get(id)
-    db.session.delete(bm)
+    p = Payment.query.get(id)
+    db.session.delete(p)
     db.session.commit()
-    return jsonify(bm.to_json())
+    return jsonify(p.to_json())
 
 
 @app.route('/status', methods=['GET'])
-def status():
-    msg = {'status': 'Ok!'}
-    return msg
-
+def healthcheck():
+    return "ok", 200
 
 if __name__ == "__main__":
     db.init_app(app)
